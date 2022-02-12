e498f9a4790544b68359652444b8d87d45b3fbb7
CJM1994 <connorjohnmullin@gmail.com>
2022-02-11 23:53:39 -0800

    completes all 4 checks

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  function sumItems(array, n) {
+
+    n = n || 0;
+
+    if (Array.isArray(array[n])) {
+      let x = 0;
+      for (const element of array[n]) {
+        x = sumItems(array[n], 0);
+      }
+      return x + sumItems(array, n + 1);
+    }
+
+    if (n < array.length) {
+      return array[n] + sumItems(array, n + 1);
+    }
+    else return 0;
+  }
+
+  module.exports = sumItems;
 }
 
 module.exports = sumItems;
